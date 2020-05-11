1e0362bbbfcb519f6172c1d1b51a359894094e9f
ZHC <bjhansen2012@outlook.com>
2020-05-11 11:58:03 +0800

    放弃对3.4的支持

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(name="flamechess",
           "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
           "Programming Language :: Python :: 3"
       ],
-      python_requires=">=3.4.0",
+      python_requires=">=3.5.0",
       )

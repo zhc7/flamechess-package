9f6db26000d33bf1543729aa485d24e7b05b1708
ZHC <bjhansen2012@outlook.com>
2020-05-11 20:07:20 +0800

    适配更改后的API
    对于jbh更新api不更新api文档的做法表示强烈谴责。。

--- flamechess/socket.py
+++ flamechess/socket.py
@@ -1,4 +1,5 @@
 import socketio
+import time
 
 
 class Client(socketio.Client):
@@ -10,35 +11,43 @@ class Client(socketio.Client):
         self.chesspos = None
         self.on("connect", self.on_connect)
         self.on("login_result", self.on_login)
+        self.on("login_fail", self.on_login_fail)
         self.on("update_chesspos", self.on_update)
         self.connect(url)
 
     def on_connect(self):
-        self.emit("login", (self.sid, {
+        self.emit("login", {
             "backend": "https://chessterm.tech",
             "userId": self.userId,
             "gameId": self.gameId
-        }))
+        })
 
     def on_login(self, data):
         self.data = data
+        print('logged in', data)
 
     def on_update(self, chesspos):
         self.chesspos = chesspos
 
+    def on_login_fail(self, msg):
+        print(msg)
+
     def set_data(self, chesspos):
-        self.emit("update_board", (self.sid, {
+        self.emit("update_board", {
             "chesspos": chesspos
-        }))
+        })
 
     def get_data(self):
         return self.chesspos
 
 
 if __name__ == '__main__':
-    client = Client(1002, 10170101)
+    client = Client('1002', '10170101')
     last_board = client.get_data()
+    print(last_board)
+    client.set_data('000000ZZZ0000000zzz000000')
     while True:
+        time.sleep(0.5)
         board = client.get_data()
         if board != last_board:
             print(board)

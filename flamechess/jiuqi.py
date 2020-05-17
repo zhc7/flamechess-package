ee27e57e111180f4617e30d816983dc6467b989e
ZHC <bjhansen2012@outlook.com>
2020-05-17 22:49:40 +0800

    加入了flag参数

--- flamechess/jiuqi.py
+++ flamechess/jiuqi.py
@@ -2,12 +2,12 @@ class Game:
     def __init__(self):
         pass
 
-    def available_actions(self, state):
+    def available_actions(self, state, flag):
         pass
 
     def next_state(self, state, action):
         pass
 
-    def end_game(self, state):
+    def end_game(self, state, flag):
         pass
 

0ad8783236807fb04fbeeff6ca533f4427d49075
ZHC <bjhansen2012@outlook.com>
2020-05-23 11:18:49 +0800

    加入把chesspos变为state

--- flamechess/zhuobie_player.py
+++ flamechess/zhuobie_player.py
@@ -11,11 +11,18 @@ class Wrapped(Client):
         self.first_run = True
 
     def on_update(self, chesspos):
+        state = []
+        change = {'Z': 1, 'z': -1, '0': 0}
+        for line in [chesspos[6:9], chesspos[11:14], chesspos[16:19]]:
+            new_line = []
+            for spot in line:
+                new_line.append(change[spot])
+            state.append(new_line)
         if self.first_run:
-            self.tree = Tree(chesspos, self.game, -1, 300)
+            self.tree = Tree(state, self.game, -1, 300)
             self.first_run = False
         else:
-            self.tree.update_by_state(chesspos)
+            self.tree.update_by_state(state)
         new_state = self.tree.search().state
         self.set_data(new_state)
 

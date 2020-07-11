01af85192dec5deb1ac7ae83b1353b204f8027b5
ZHC <bjhansen2012@outlook.com>
2020-07-11 10:16:17 +0800

    添加治疗眼瞎工具

---
+++ flamechess/beautifulPrint.py
@@ -0,0 +1,26 @@
+import sys
+sysprint = print
+
+
+def beautifulPrint(state, action):
+    printC = sys.stdout.shell.write
+    route = sum(action[0], ())
+    eat = sum(action[1], ())
+    for x in range(len(state)):
+        row = state[x]
+        for y in range(len(row)):
+            col = row[y]
+            if (x, y) in route:
+                print = lambda x: printC((x or '#') + ' ', "STRING")
+            elif (x, y) in eat:
+                print = lambda x: printC((x or '#') + ' ', "KEYWORD")
+            else:
+                print = lambda x: sysprint((x or ' '), end=' ')
+            if col == -1:
+                print('X')
+            elif col == 1:
+                print('O')
+            else:
+                print('')
+            print = sysprint
+        print()

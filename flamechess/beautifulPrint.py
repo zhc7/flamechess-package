7a39114b64f627ce9245640ca331a7a4a257ede5
张瀚宸 <bjhansen2012@outlook.com>
2020-07-16 11:25:24 +0800

    更新beautifulPrint

--- flamechess/beautifulPrint.py
+++ flamechess/beautifulPrint.py
@@ -24,3 +24,58 @@ def beautifulPrint(state, action):
                 print('')
             print = sysprint
         print()
+
+
+def log_parser(logFile):
+    try:
+        with open(logFile) as f:
+            log = f.readlines()
+    except FileNotFoundError:
+        log = logFile.split('\n')
+    states = []
+    actions = []
+    state = []
+    for line in log:
+        if not line or line[:2] not in ["O ", "X ", "- "]:
+            if line[0] == '(':
+                actions.append(line)
+            if state:
+                states.append(state)
+            state = []
+            continue
+        row = []
+        for item in line.split(' '):
+            item = item.strip()
+            if not item:
+                continue
+            row.append({"O":1, "X":-1, "-":0}[item])
+        state.append(row)
+    return states, actions
+
+
+def differencePrinter(states, actions=None):
+    last_state = states[0]
+    printC = sys.stdout.shell.write
+    for i in range(len(states)):
+        state = states[i]
+        if not state:
+            continue
+        for x in range(len(state)):
+            row = state[x]
+            last_row = last_state[x]
+            for y in range(len(row)):
+                col = row[y]
+                last_col = last_row[y]
+                if col != last_col:
+                    printC({1:'O', -1:'X', 0:'-'}[col] + " ", "KEYWORD")
+                else:
+                    printC({1: 'O', -1: 'X', 0: '-'}[col] + " ")
+            print()
+        if actions:
+            print(actions[i])
+        last_state = state
+        input()
+
+
+
+
